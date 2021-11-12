093f1d7602c70100a6a8d8f23e95b7648cfb4eb6
Enda Crossan <enda@endacrossan.com>
2021-11-12 16:06:53 +0000

    initial commit, index, css and js

---
+++ js/script.js
@@ -0,0 +1,5 @@
+// this function is used to change the background colour of the page
+// it is called when you hover on one of the lights
+function changePageColour(colour) {
+    document.body.style.backgroundColor = colour;
+}
